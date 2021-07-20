a6e20d4ad0950734515aef194532d4331f8bce5f
Elton Souza <elton.souza1515@gmail.com>
2021-07-20 11:03:03 -0300

    fetch API

--- JS Web/CRUD/projeto_inicial/admin/README.md
+++ JS Web/CRUD/projeto_inicial/admin/README.md
@@ -5,7 +5,7 @@ instalar as dependencias: json-server
 - npm install;
 e rodar o json server: 
 ```js
-jsx json-server --watch db.json
+npx json-server --watch db.json
 ```
 
 Rodar: browser-sync start --server --file . --host --port 5000 --startPath admin/telas/lista_cliente.html
--- JS Web/CRUD/projeto_inicial/admin/service/cliente-service.js
+++ JS Web/CRUD/projeto_inicial/admin/service/cliente-service.js
@@ -14,15 +14,35 @@ function criaNovaLinha(nome,email){
     return linhaNovoCliente
 }
 
-const http = new XMLHttpRequest() // Objeto que fornece métodos para a comunicação
+const listaClientes = () =>{
+    return fetch('http://localhost:3000/profile') // faz um get e devolve uma promise
+    .then(answer =>{ // trabalhando com a resposta
+        const resposta = answer.json() // transforma a resposta de texto em um objeto javascript válido
+        console.log(resposta)
+        return resposta 
+    })
 
-http.open('GET','http://localhost:3000/profile') // método que abre a comunicação entre a aplicação e a API -> ('o que pedir para o servidor','endereço')
+    // const promise = new Promise((resolve,reject)=>{
+    //     const http = new XMLHttpRequest() // Objeto que fornece métodos para a comunicação
 
-http.send() // Envia a requisição para o servidor
+    //     http.open('GET','http://localhost:3000/profile') // método que abre a comunicação entre a aplicação e a API -> ('o que pedir para o servidor','endereço')
 
-http.onload = () => { // executa ao recarregar a página
-    const data = JSON.parse(http.response) // dados recebidos pelo servidor(resposta em texto)
+    //     http.onload = () => { // executa ao recarregar a página
+    //         if(http.dtatus >= 400){ // verifica se está tudo ok com a chamada, se estiver algum erro cliente/servidor (ex:400) ele rejeita a resposta
+    //             reject(JSON.parse(http.response))
+    //         }else{ // se nao ele aceita
+    //             resolve(JSON.parse(http.response))
+    //         }
+    //     }
+
+    //     http.send() // Envia a requisição para o servidor
+    // })
+    // return promise 
+}
+
+listaClientes()
+.then(data => { //referencia a listaClientes()
     data.forEach(cliente=>{
         tabela.appendChild(criaNovaLinha(cliente.nome,cliente.email))
     })
-}
+})
