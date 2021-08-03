499639d75d13cede2ba7f9377b74f5c9a88abda7
Elton Souza <elton.souza1515@gmail.com>
2021-08-03 16:00:59 -0300

    validacao form

---
+++ JS Web/Validacao Formulario/js/app.js
@@ -0,0 +1,6 @@
+import {valida} from "./validacao.js";
+
+const inputs = document.querySelectorAll('input')
+inputs.forEach(input =>{
+    input.addEventListener('blur', (evento)=> valida(evento.target) )
+})
---
+++ JS Web/Validacao Formulario/js/validacao.js
@@ -0,0 +1,30 @@
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input)
+    }
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido')
+    }else{
+        input.parentElement.classList.add('input-container--invalido')
+    }
+}
+const validadores ={
+    dataNascimento: input => validaDataNascimento(input)
+}
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+    if(!maiorQue18(dataRecebida)){
+        mensagem = "Voce deve ser maior que 18 anos para se cadastrar."
+    }
+    input.setCustomValidity(mensagem) // validacao customizada do input
+}
+
+function maiorQue18(data){
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    // console.log(dataAtual, dataMais18)
+    return dataMais18 <= dataAtual
+}
